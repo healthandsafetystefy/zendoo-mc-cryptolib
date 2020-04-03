bfec5053fa375a10c97a0df2797ab2ec3deb0e76
DanieleDiBenedetto <daniele@horizenlabs.io>
2020-04-03 13:13:30 -0400

    Updated with new design. Still missing few functions

--- include/zendoo_mc.h
+++ include/zendoo_mc.h
@@ -35,6 +35,33 @@ extern "C" {
     //Free memory from allocated pk
     void zendoo_pk_free(pk_t* pk);
 
+//Sk related functions
+
+    typedef struct sk sk_t;
+
+    //Get the byte size of sk
+    size_t zendoo_get_sk_size_in_bytes(void);
+
+    //Serialize a sk into sk_bytes given an opaque pointer to it
+    bool zendoo_serialize_sk(
+        const sk_t*    sk,
+        unsigned char* sk_bytes
+    );
+
+    //Get an opaque pointer to a sk built from its byte serialization
+    sk_t* zendoo_deserialize_sk(const unsigned char* sk_bytes);
+
+    //Free memory from allocated sk
+    void zendoo_sk_free(sk_t* sk);
+
+
+//Keypair related functions
+
+    typedef struct {
+        pk_t* pk;
+        sk_t* sk;
+    } keypair_t ;
+
 //SNARK related functions
 
     typedef struct ginger_zk_proof ginger_zk_proof_t;
@@ -66,11 +93,6 @@ extern "C" {
     field_t* zendoo_compute_poseidon_hash(
         const field_t** input,
         size_t input_len
-    );
-
-    field_t* zendoo_compute_keys_hash_commitment(
-        const pk_t** pks,
-        size_t pk_num
     );
 
 //Poseidon-based Merkle Tree related functions
@@ -107,6 +129,103 @@ extern "C" {
         ginger_mt_path_t* path
     );
 
+//Schnorr signature related functions
+
+    typedef struct schnorr_sig schnorr_sig_t;
+
+    keypair_t zendoo_schnorr_keygen(void);
+
+    pk_t* zendoo_schnorr_get_pk(const sk_t* sk);
+
+    bool zendoo_schnorr_key_verify(pk_t* pk);
+
+    schnorr_sig_t* zendoo_schnorr_sign(
+        const field_t** message,
+        size_t message_len,
+        const keypair_t keys
+    );
+
+    bool zendoo_schnorr_verify(
+        field_t** message,
+        size_t message_len,
+        pk_t* pk,
+        schnorr_sig_t* sig
+    );
+
+    //Get the byte size of sign
+    size_t zendoo_get_schnorr_sig_size_in_bytes(void);
+
+    //Serialize a schnorr sig into sig_bytes given an opaque pointer to it
+    bool zendoo_serialize_schnorr_sig(
+        const schnorr_sig_t* sig,
+        unsigned char* sig_bytes
+    );
+
+    //Get an opaque pointer to a schnorr sig built from its byte serialization
+    schnorr_sig_t* zendoo_deserialize_schnorr_sig(const unsigned char* sig_bytes);
+
+    //Free memory from allocated schnorr sig
+    void zendoo_schnorr_sig_free(schnorr_sig_t* sig);
+
+//VRF related functions
+
+    typedef struct ecvrf_proof ecvrf_proof_t;
+
+    keypair_t zendoo_ecvrf_keygen(void);
+
+    pk_t* zendoo_ecvrf_get_pk(sk_t* sk);
+
+    bool zendoo_ecvrf_key_verify(pk_t* pk);
+
+    ecvrf_proof_t* zendoo_ecvrf_prove(
+        const field_t** message,
+        size_t message_len,
+        const keypair_t keys
+    );
+
+    field_t* zendoo_ecvrf_proof_to_hash(
+        field_t** message,
+        size_t message_len,
+        pk_t* pk,
+        ecvrf_proof_t* proof
+    );
+
+    //Get the byte size of a ecvrf proof
+    size_t zendoo_get_ecvrf_proof_size_in_bytes(void);
+
+    //Serialize an ecvrf proof into ecvrf_proof_bytes given an opaque pointer to it
+    bool zendoo_serialize_ecvrf_proof(
+        const ecvrf_proof_t* ecvrf_proof,
+        unsigned char* proof_bytes
+    );
+
+    //Get an opaque pointer to an ecvrf_proof built from its byte serialization
+    ecvrf_proof_t* zendoo_deserialize_ecvrf_proof(const unsigned char* proof_bytes);
+
+    //Free memory from allocated ecvrf proof
+    void zendoo_ecvrf_proof_free(ecvrf_proof_t* proof);
+
+//Naive threshold sig circuit related functions
+
+    ginger_zk_proof_t* zendoo_create_naive_threshold_sig_proof(
+        const uint8_t* params_path,
+        size_t params_path_len,
+        const pk_t** pks,
+        size_t pk_num,
+        const schnorr_sig_t** sigs,
+        size_t sig_num,
+        const field_t* threshold,
+        const field_t* b,
+        const field_t* message,
+        const field_t* hash_commitment,
+        size_t n
+    );
+
+    field_t* zendoo_compute_keys_hash_commitment(
+        const pk_t** pks,
+        size_t pk_num
+    );
+
 //Test functions
 
     //Get an opaque pointer to a random field element
@@ -121,6 +240,22 @@ extern "C" {
         const pk_t* pk_1,
         const pk_t* pk_2
     );
-}
 
+    sk_t* zendoo_get_random_sk(void);
+
+    bool zendoo_sk_assert_eq(
+        const sk_t* sk_1,
+        const sk_t* sk_2
+    );
+
+    bool zendoo_schnorr_sig_assert_eq(
+        const schnorr_sig_t* sig_1,
+        const schnorr_sig_t* sig_2
+    );
+
+    bool zendoo_ecvrf_proof_assert_eq(
+        const ecvrf_proof_t* proof_1,
+        const ecvrf_proof_t* proof_2
+    );
+}
 #endif // ZENDOO_MC_INCLUDE_H_
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,34 @@
-use algebra::{
-    fields::mnt4753::Fr,
-    curves::{
-        mnt4753::MNT4 as PairingCurve,
-        mnt6753::G1Affine
-    },
-    bytes::{FromBytes, ToBytes},
-    UniformRand
-};
+use algebra::{fields::mnt4753::{Fr, Fq as Fs}, curves::{
+    mnt4753::MNT4 as PairingCurve,
+    mnt6753::{G1Affine, G1Projective},
+    ProjectiveCurve,
+}, bytes::{FromBytes, ToBytes}, UniformRand, AffineCurve};
 
 use primitives::{
     crh::{
         FieldBasedHash, MNT4PoseidonHash as FrHash,
+        bowe_hopwood::{BoweHopwoodPedersenCRH, BoweHopwoodPedersenParameters},
     },
     merkle_tree::field_based_mht::{
         FieldBasedMerkleHashTree, FieldBasedMerkleTreeConfig, FieldBasedMerkleTreePath
     },
+    signature::{
+        schnorr::field_based_schnorr::{FieldBasedSchnorrSignature, FieldBasedSchnorrSignatureScheme},
+        FieldBasedSignatureScheme,
+    },
+    vrf::{
+        FieldBasedVrf,
+        ecvrf::{
+            FieldBasedEcVrf, FieldBasedEcVrfProof,
+        }
+    },
 };
 
 use proof_systems::groth16::{Proof, verifier::verify_proof, prepare_verifying_key};
+
+use demo_circuit::constants::{
+    VRFParams, VRFWindow,
+};
 
 use rand::rngs::OsRng;
 use libc::{
@@ -29,9 +40,18 @@ use std::{
         Error as IoError, ErrorKind,
     },
 };
+use lazy_static::*;
 
 pub mod error;
 use error::*;
+
+lazy_static! {
+    pub static ref VRF_GH_PARAMS: BoweHopwoodPedersenParameters<G1Projective> = {
+        let params = VRFParams::new();
+        BoweHopwoodPedersenParameters::<G1Projective>{generators: params.group_hash_generators}
+    };
+}
+
 
 #[cfg(test)]
 pub mod tests;
@@ -39,10 +59,13 @@ pub mod tests;
 // ************CONSTANTS******************
 
 const FR_SIZE: usize = 96;
+const FS_SIZE: usize = FR_SIZE; // 96
 const G1_SIZE: usize = 193;
 const G2_SIZE: usize = 385;
 
 const GROTH_PROOF_SIZE: usize = 2 * G1_SIZE + G2_SIZE;  // 771
+const SIG_SIZE:         usize = 2 * FR_SIZE;            // 192
+const VRF_PROOF_SIZE:   usize = G1_SIZE + 2 * FR_SIZE;  // 385
 
 // ************TYPES**********************
 
@@ -55,7 +78,11 @@ impl FieldBasedMerkleTreeConfig for ZendooMcFieldBasedMerkleTreeParams {
 
 type GingerMerkleTree = FieldBasedMerkleHashTree<ZendooMcFieldBasedMerkleTreeParams>;
 type GingerMerkleTreePath = FieldBasedMerkleTreePath<ZendooMcFieldBasedMerkleTreeParams>;
-
+type SchnorrSig = FieldBasedSchnorrSignature<Fr>;
+type SchnorrSigScheme = FieldBasedSchnorrSignatureScheme<Fr, G1Projective, FrHash>;
+type GroupHash = BoweHopwoodPedersenCRH<G1Projective, VRFWindow>;
+type EcVrfProof = FieldBasedEcVrfProof<Fr, G1Projective>;
+type EcVrfScheme = FieldBasedEcVrf<Fr, G1Projective, FrHash, GroupHash>;
 type GingerProof = Proof<PairingCurve>;
 
 // ***********UTILITY FUNCTIONS*************
@@ -167,7 +194,24 @@ pub extern "C" fn zendoo_field_free(field: *mut Fr)
     drop(unsafe { Box::from_raw(field) });
 }
 
+//Keypair struct declaration
+
+#[repr(C)]
+pub struct KeyPair{
+    pk: *mut G1Affine,
+    sk: *mut Fs,
+}
+
 //***********Pk functions****************
+#[no_mangle]
+pub extern "C" fn zendoo_get_pk_size_in_bytes() -> c_uint { G1_SIZE as u32 }
+
+#[no_mangle]
+pub extern "C" fn zendoo_serialize_pk(
+    pk:            *const G1Affine,
+    result:        *mut [c_uchar; G1_SIZE]
+) -> bool
+{ serialize_to_buffer(pk, &mut (unsafe { &mut *result })[..], G1_SIZE, "pk") }
 
 #[no_mangle]
 pub extern "C" fn zendoo_deserialize_pk(
@@ -180,6 +224,57 @@ pub extern "C" fn zendoo_pk_free(pk: *mut G1Affine)
 {
     if pk.is_null()  { return }
     drop(unsafe { Box::from_raw(pk) });
+}
+
+//********************Sk functions***********************
+
+#[no_mangle]
+pub extern "C" fn zendoo_get_sk_size_in_bytes() -> c_uint { FS_SIZE as u32 }
+
+#[no_mangle]
+pub extern "C" fn zendoo_serialize_sk(
+    sk:            *const Fs,
+    result:        *mut [c_uchar; FS_SIZE]
+) -> bool
+{
+    if sk.is_null() {
+        set_last_error(Box::new(NullPointerError(format!("Null sk"))), NULL_PTR_ERROR);
+        return false
+    }
+    let sk = unsafe { &*sk };
+    match sk.write(&mut (unsafe { &mut *result })[..]) {
+        Err(_) => {
+            let err = IoError::new(ErrorKind::InvalidData, format!("result should be {} bytes", FS_SIZE));
+            set_last_error(Box::new(err), IO_ERROR);
+            false
+        }
+        Ok(_) => true
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_deserialize_sk(
+    sk_bytes:    *const [c_uchar; FS_SIZE]
+) -> *mut Fs
+{
+    //Read pk
+    let sk_bytes = unsafe{&* sk_bytes};
+    let sk = match Fs::read(&sk_bytes[..]) {
+        Ok(fe) => fe,
+        Err(e) => {
+            set_last_error(Box::new(e), IO_ERROR);
+            return null_mut()
+        },
+    };
+
+    Box::into_raw(Box::new(sk))
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_sk_free(sk: *mut Fs)
+{
+    if sk.is_null()  { return }
+    drop(unsafe { Box::from_raw(sk) });
 }
 
 //********************SNARK functions********************
@@ -421,6 +516,318 @@ pub extern "C" fn ginger_mt_path_free(path: *mut GingerMerkleTreePath) {
     drop(unsafe { Box::from_raw(path) });
 }
 
+// ********************Signature functions********************
+
+#[no_mangle]
+pub extern "C" fn zendoo_schnorr_keygen() -> KeyPair
+{
+    let mut rng = OsRng::default();
+    let (pk, sk) = SchnorrSigScheme::keygen(&mut rng);
+    KeyPair {
+        pk: Box::into_raw(Box::new(pk.into_affine())),
+        sk: Box::into_raw(Box::new(sk)),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_schnorr_get_pk(
+    sk: *const Fs
+) -> *mut G1Affine
+{
+    if sk.is_null() {
+        let e = Box::new(NullPointerError(format!("Null sk")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return null_mut()
+    }
+    let pk = SchnorrSigScheme::get_public_key(unsafe {&*sk});
+    Box::into_raw(Box::new(pk.into_affine()))
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_schnorr_key_verify(
+    pk: *const G1Affine
+) -> bool
+{
+    if pk.is_null() {
+        let e = Box::new(NullPointerError(format!("Null pk")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return false
+    }
+    SchnorrSigScheme::keyverify(&(unsafe { &* pk}.into_projective()))
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_schnorr_sign(
+    message:     *const *const Fr,
+    message_len: usize,
+    keypair:     KeyPair
+) -> *mut SchnorrSig
+{
+    let mut rng = OsRng::default();
+
+    //Read message as field elements
+    let fes = match read_double_raw_pointer(message, message_len){
+        Some(fes) => fes,
+        None => return null_mut()
+    };
+
+    let (pk, sk) = unsafe {(&*keypair.pk, &*keypair.sk)};
+
+    //Sign message and return opaque pointer to sig
+    match SchnorrSigScheme::sign(&mut rng, &pk.into_projective(), sk, fes.as_slice()) {
+        Ok(sig) => Box::into_raw(Box::new(sig)),
+        Err(e) => {
+            set_last_error(e, CRYPTO_ERROR);
+            null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_schnorr_verify(
+    message:     *const *const Fr,
+    message_len: usize,
+    pk:          *const G1Affine,
+    sig:         *const SchnorrSig,
+) -> bool
+{
+    //Read message as field elements
+    let fes = match read_double_raw_pointer(message, message_len){
+        Some(fes) => fes,
+        None => return false
+    };
+
+    //Read pk
+    if pk.is_null(){
+        let e = Box::new(NullPointerError(format!("Null Pk")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return false
+    }
+    let pk = unsafe{ &*pk }.into_projective();
+
+    //Read sig
+    if sig.is_null(){
+        let e = Box::new(NullPointerError(format!("Null sig")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return false
+    }
+    let sig = unsafe{ &*sig };
+
+    //Verify sig
+    match SchnorrSigScheme::verify(&pk, fes.as_slice(), sig) {
+        Ok(result) => result,
+        Err(e) => {
+            set_last_error(e, CRYPTO_ERROR);
+            false
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_get_schnorr_sig_size_in_bytes() -> c_uint { SIG_SIZE as u32 }
+
+#[no_mangle]
+pub extern "C" fn zendoo_serialize_schnorr_sig(
+    sig: *const SchnorrSig,
+    result: *mut [c_uchar; SIG_SIZE],
+) -> bool
+{
+    if sig.is_null() {
+        set_last_error(Box::new(NullPointerError(format!("Null sig"))), NULL_PTR_ERROR);
+        return false
+    }
+    let sig = unsafe { &*sig };
+    match sig.write(&mut (unsafe { &mut *result })[..]) {
+        Err(_) => {
+            let err = IoError::new(ErrorKind::InvalidData, format!("result should be {} bytes", SIG_SIZE));
+            set_last_error(Box::new(err), IO_ERROR);
+            false
+        }
+        Ok(_) => true
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_deserialize_schnorr_sig(
+    sig_bytes: *const [c_uchar; SIG_SIZE]
+) -> *mut SchnorrSig
+{
+    //Read sig
+    let sig_bytes = unsafe{&* sig_bytes};
+    let sig = match SchnorrSig::read(&sig_bytes[..]) {
+        Ok(sig) => sig,
+        Err(e) => {
+            set_last_error(Box::new(e), IO_ERROR);
+            return null_mut()
+        },
+    };
+
+    Box::into_raw(Box::new(sig))
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_schnorr_sig_free(sig: *mut SchnorrSig)
+{
+    if sig.is_null() { return }
+    drop(unsafe{ Box::from_raw(sig) });
+}
+
+// ********************VRF functions********************
+
+#[no_mangle]
+pub extern "C" fn zendoo_ecvrf_keygen() -> KeyPair
+{
+    let mut rng = OsRng::default();
+    let (pk, sk) = SchnorrSigScheme::keygen(&mut rng);
+    KeyPair {
+        pk: Box::into_raw(Box::new(pk.into_affine())),
+        sk: Box::into_raw(Box::new(sk)),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_ecvrf_get_pk(
+    sk: *const Fs
+) -> *mut G1Affine
+{
+    if sk.is_null() {
+        let e = Box::new(NullPointerError(format!("Null sk")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return null_mut()
+    }
+    let pk = EcVrfScheme::get_public_key(unsafe {&*sk});
+    Box::into_raw(Box::new(pk.into_affine()))
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_ecvrf_key_verify(
+    pk: *const G1Affine
+) -> bool
+{
+    if pk.is_null() {
+        let e = Box::new(NullPointerError(format!("Null pk")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return false
+    }
+    EcVrfScheme::keyverify(&(unsafe { &* pk}.into_projective()))
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_ecvrf_prove(
+    message:     *const *const Fr,
+    message_len: usize,
+    keypair:     KeyPair
+) -> *mut EcVrfProof
+{
+    let mut rng = OsRng::default();
+
+    //Read message as field elements
+    let fes = match read_double_raw_pointer(message, message_len){
+        Some(fes) => fes,
+        None => return null_mut()
+    };
+
+    let (pk, sk) = unsafe {(&*keypair.pk, &*keypair.sk)};
+
+    //Generate proof for message and return opaque pointer to proof
+    match EcVrfScheme::prove(&mut rng, &VRF_GH_PARAMS, &pk.into_projective(), sk, fes.as_slice()) {
+        Ok(sig) => Box::into_raw(Box::new(sig)),
+        Err(e) => {
+            set_last_error(e, CRYPTO_ERROR);
+            null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_ecvrf_proof_to_hash(
+    message:     *const *const Fr,
+    message_len: usize,
+    pk:          *const G1Affine,
+    proof:       *const EcVrfProof,
+) -> *mut Fr
+{
+    //Read message as field elements
+    let fes = match read_double_raw_pointer(message, message_len){
+        Some(fes) => fes,
+        None => return null_mut()
+    };
+
+    //Read pk
+    if pk.is_null(){
+        let e = Box::new(NullPointerError(format!("Null Pk")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return null_mut()
+    }
+    let pk = unsafe{ &*pk }.into_projective();
+
+    //Read proof
+    if proof.is_null(){
+        let e = Box::new(NullPointerError(format!("Null proof")));
+        set_last_error(e, NULL_PTR_ERROR);
+        return null_mut()
+    }
+    let proof = unsafe{ &*proof };
+
+    //Verify proof and return VRF output
+    match EcVrfScheme::proof_to_hash(&VRF_GH_PARAMS, &pk, fes.as_slice(), proof) {
+        Ok(result) => Box::into_raw(Box::new(result)),
+        Err(e) => {
+            set_last_error(e, CRYPTO_ERROR);
+            null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_get_ecvrf_proof_size_in_bytes() -> c_uint { VRF_PROOF_SIZE as u32 }
+
+#[no_mangle]
+pub extern "C" fn zendoo_serialize_ecvrf_proof(
+    proof:  *const EcVrfProof,
+    result: *mut [c_uchar; VRF_PROOF_SIZE],
+) -> bool
+{
+    if proof.is_null() {
+        set_last_error(Box::new(NullPointerError(format!("Null proof"))), NULL_PTR_ERROR);
+        return false
+    }
+    let proof = unsafe { &*proof };
+    match proof.write(&mut (unsafe { &mut *result })[..]) {
+        Err(_) => {
+            let err = IoError::new(ErrorKind::InvalidData, format!("result should be {} bytes", VRF_PROOF_SIZE));
+            set_last_error(Box::new(err), IO_ERROR);
+            false
+        }
+        Ok(_) => true
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_deserialize_ecvrf_proof(
+    proof_bytes: *const [c_uchar; VRF_PROOF_SIZE]
+) -> *mut EcVrfProof
+{
+    //Read proof
+    let proof_bytes = unsafe{&* proof_bytes};
+    let proof = match EcVrfProof::read(&proof_bytes[..]) {
+        Ok(proof) => proof,
+        Err(e) => {
+            set_last_error(Box::new(e), IO_ERROR);
+            return null_mut()
+        },
+    };
+
+    Box::into_raw(Box::new(proof))
+}
+
+#[no_mangle]
+pub extern "C" fn zendoo_ecvrf_proof_free(proof: *mut EcVrfProof)
+{
+    if proof.is_null() { return }
+    drop(unsafe{ Box::from_raw(proof) });
+}
+
 //***************Test functions*******************
 
 fn check_equal<T: Eq>(val_1: *const T, val_2: *const T) -> bool{
@@ -441,6 +848,13 @@ pub extern "C" fn zendoo_field_assert_eq(
     field_1: *const Fr,
     field_2: *const Fr,
 ) -> bool { check_equal(field_1, field_2 )}
+
+#[no_mangle]
+pub extern "C" fn zendoo_get_random_pk() -> *mut G1Affine {
+    let mut rng = OsRng;
+    let random_g = G1Projective::rand(&mut rng);
+    Box::into_raw(Box::new(random_g.into_affine()))
+}
 
 #[no_mangle]
 pub extern "C" fn zendoo_pk_assert_eq(
